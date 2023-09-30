c9ceb2a0aa3ed3ae747231a29eea90a2deee2a65
NachiketSaraf <sarafnachiket@gmail.com>
2023-09-30 21:49:15 +0530

    Added home page in icon component

--- nexus-ui/material-dashboard-angular2-master/src/app/app.module.ts
+++ nexus-ui/material-dashboard-angular2-master/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   imports: [
@@ -17,6 +18,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
+    MatCardModule
   ],
   declarations: [
     AppComponent,
